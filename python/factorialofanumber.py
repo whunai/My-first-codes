6089adf33ecfc9c69ee471eb95f3ce0504b81368
whunai <128331509+whunai@users.noreply.github.com>
2024-09-05 18:26:23 -0300

    Atualização
    Arquivos atualizados:
    - factorialofanumber v3
    - guessthenumber v2
    
    Atualizações, correções e melhorias no código para encontrar o fatorial de um número.

--- python/factorialofanumber.py
+++ python/factorialofanumber.py
@@ -1,26 +1,36 @@
 # Fatorial de um número
 
-ChosenValue = input('Insira aqui o número que você deseja encontrar o valor fatorial: ')
+ChosenValue = input("Insira aqui o número que você deseja encontrar o valor fatorial: ")
 
 total = 1
 
 def hasSingleComma(comma):
     return comma.count(',') == 1
 
-if str(ChosenValue).isalpha():
-    print("Valor inserido é inválido.")
+def hasSingleDot(dot):
+    return dot.count('.') == 1
 
-elif hasSingleComma(ChosenValue):
-    print("Não há fatorial de valores decimais.")
+letters = tuple(chr(letter) for letter in range(32, 127) if not (48 <= letter <= 57 or letter in {44, 45, 46}))
+def hasSingleSymbol(symbol):
+    return symbol in letters
 
-elif ChosenValue.isnumeric() and float(ChosenValue).is_integer() == True:
-    if int(ChosenValue) >= 0:
+numbers = tuple(chr(numero) for numero in range(48, 58)) # Mesmo esquema da de cima, mas apenas com números
+def hasSingleNumber(decimalScale):
+    return decimalScale in numbers
+
+while True:
+    if any(hasSingleSymbol(char) for char in ChosenValue) == False and hasSingleComma(ChosenValue) and any(hasSingleNumber(char) for char in ChosenValue) or any(hasSingleSymbol(char) for char in ChosenValue) == False and hasSingleDot(ChosenValue) and any(hasSingleNumber(char) for char in ChosenValue):
+        ChosenValue = input("O valor inserido precisa ser inteiro. Por favor, insira novamente o número para descobrir seu fatorial: ")
+    
+    elif any(hasSingleSymbol(char) for char in ChosenValue) or any(hasSingleSymbol(char) for char in ChosenValue) and any(hasSingleNumber(char) for char in ChosenValue):
+        ChosenValue = input("Valor inserido é inválido. Por favor, insira novamente o número para descobrir seu fatorial: ")
+    
+    elif ChosenValue.isnumeric() and float(ChosenValue).is_integer() == True and int(ChosenValue) >= 0:
         for multiplier in range(1, int(ChosenValue) + 1):
             total = total * multiplier
         print("O fatorial de "+ChosenValue+" é "+str(total)+".")
+        break
     elif int(ChosenValue) < 0:
-        print("Não há fatorial de números negativos.")
-elif float(ChosenValue).is_integer() == False:
-    print("Não há fatorial de valores decimais.")
-else:
-    print("Valor inserido é inválido.")
+        ChosenValue = input("Não há fatorial de valores negativos. Por favor, insira novamente o número para descobrir seu fatorial: ")
+    else:
+        ChosenValue = input("Valor inserido é inválido. Por favor, insira novamente o número para descobrir seu fatorial: ")
--- python/guessthenumber.py
+++ python/guessthenumber.py
@@ -1,44 +1,33 @@
-# Biblioteca de aleatoriedade do Python sendo inportada
 import random
 
-randomValue = random.randint(1, 10) # Variável com valor aleatório no intervalo 1-10 sendo definida
-guessedValue = input("Estou pensando em um número de 1 à 10, tente adivinhar qual é! Palpite: ") # Recebendo uma entrada para a variável guessedValue
+randomValue = random.randint(1, 10) 
+guessedValue = input("Estou pensando em um número de 1 à 10, tente adivinhar qual é! Palpite: ") 
 
-# Detector de vírgulas 
-def hasSingleComma(comma): # Definindo uma função que verifica se dentro de uma string genérica tem pelo menos 1 vírgula
-    return comma.count(',') == 1 # Aqui o "return" retorna o valor para o código que chamou a função
+def hasSingleComma(comma):
+    return comma.count(',') == 1
 
-# Detector de pontos
-def hasSingleDot(dot): # "dot" e "comma" São strings genéricas que são substituídas pela string especificada quando a função é chamada
+def hasSingleDot(dot):
     return dot.count('.') == 1
 
-# Detector de caracteres
-letters = tuple(chr(letter) for letter in range(32, 127) if not (48 <= letter <= 57)) # Aqui a variável "letter" chama uma coleção por meio do for, exceto pelos valores de 48 pra cima e 57 pra baixo (que representam os números).
-# O "chr(letter)" pega cada um desses números e converte num caractere ASCII e o tuple transforma a variável em uma tupla, fazendo uma lista com cada caractere separado invidualmente
+letters = tuple(chr(letter) for letter in range(32, 127) if not (48 <= letter <= 57 or letter in {44, 45, 46}))
 def hasSingleSymbol(symbol):
-    return symbol in letters # Aqui ele verifica a variável genérica e retorna um valor para o código que chamou a função caso a string possua algum desses caracteres.
+    return symbol in letters
 
-# Detector de números
-numbers = tuple(chr(numero) for numero in range(48, 58)) # Mesmo esquema da de cima, mas apenas com números
+numbers = tuple(chr(numero) for numero in range(48, 58))
 def hasSingleNumber(decimalScale):
     return decimalScale in numbers
 
 while True:
-    if hasSingleComma(guessedValue) or hasSingleDot(guessedValue):
-        guessedValue = input("O valor inserido precisa ser inteiro. Por favor, tente novamente. Palpite: ") # Aqui eu verifico se o valor de entrada é ou não decimal, para uma mensagem dedicada só pra essa situação
-    
+    if any(hasSingleSymbol(char) for char in guessedValue) == False and hasSingleComma(guessedValue) and any(hasSingleNumber(char) for char in guessedValue) or any(hasSingleSymbol(char) for char in guessedValue) == False and hasSingleDot(guessedValue) and any(hasSingleNumber(char) for char in guessedValue):
+        guessedValue = input("O valor inserido precisa ser inteiro. Por favor, tente novamente. Palpite: ") 
     elif any(hasSingleSymbol(char) for char in guessedValue) or any(hasSingleSymbol(char) for char in guessedValue) and any(hasSingleNumber(char) for char in guessedValue):
-        guessedValue = input("Valor inserido é inválido. Por favor, tente novamente. Palpite: ") # Aqui eu verifico se o valor de entrada é alfanumérico ou apenas alfa, independemente não será aceito nenhum dos dois
-    
+        guessedValue = input("Valor inserido é inválido. Por favor, tente novamente. Palpite: ")
     elif guessedValue.isnumeric() and float(guessedValue).is_integer() == True and int(guessedValue) > 10 or int(guessedValue) < 0:
         guessedValue = input("O valor inserido deve respeitar o intervalo de 1 à 10. Por favor, tente novamente. Palpite: ")
-        
     elif guessedValue.isnumeric() and float(guessedValue).is_integer() == True and int(guessedValue) == int(randomValue):
         print("Parabéns! Você acertou.")
         break
-    
     elif guessedValue.isnumeric() and float(guessedValue).is_integer() == True and int(guessedValue) > int(randomValue):
         guessedValue = input("Muito alto! Tente novamente: ")
-    
     elif guessedValue.isnumeric() and float(guessedValue).is_integer() == True and int(guessedValue) < int(randomValue):
         guessedValue = input("Muito baixo! Tente novamente: ")
