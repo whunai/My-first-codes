2480057b4d1170d92941ebdd4da51ea1b3cc8861
whunai <128331509+whunai@users.noreply.github.com>
2024-09-10 14:56:04 -0300

    besteira
    Só besteira.

---
+++ python/maetemcafe.py
@@ -0,0 +1,17 @@
+maetemcafe = input("Mãe, tem café? Resposta: ")
+
+while True:
+    if str(maetemcafe) == "Não":
+        print("Então eu vou comer abroba com leite")
+        break
+    elif str(maetemcafe) == "Não!":
+        print("pois vou criar um universo repleto de dor e sofrimento, este mundo que estou prestes a criar, será um espelho distorcido de tudo que ja vivi,de cada cicatriz que carrego na alma, vou dar vida a um universo onde a felicidade onde a felicidade não passa de uma lembrança distante, uma sombra fugaz que nunca se materializa, neste universo as pessoas que me fizeram mal, estarão presas em um ciclo interminável de dor, lutando contra forças que não podem vencer...")
+        break
+    elif str(maetemcafe) == "Não...":
+        print("O vazio dentro de mim é como uma xicara de café esquecida no canto de uma mesa, o aroma que um dia foi reconfortante agora se dispersa no ar deixando apenas o amargor de uma bebida que se esfriou com o tempo, cada gole me lembra da ausência de calor da solidão que invade o corpo e a alma...")
+        break
+    elif str(maetemcafe) == "Sim":
+        print("A alegria dentro de mim é como uma xícara de café, sempre presente no canto da mesa. O aroma que um dia foi desalentador agora se espalha pelo ar, trazendo memórias doces e acolhedoras. Cada gole me lembra do calor e da companhia que preenchem o meu corpo e alma, trazendo uma sensação de conforto e felicidade.")
+        break
+    else:
+        maetemcafe = input("Valor inválido. Tente novamente: ")
